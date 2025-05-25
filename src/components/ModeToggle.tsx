d93b368f0bca037503bc4dfd6a2ca6dc74c4df2f
GrzegorzStacel <stacelgrzegorz@gmail.com>
2025-05-25 03:11:40 +0200

    Add theme and button

---
+++ src/components/ModeToggle.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import * as React from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+
+import { Button } from "@/components/ui/button";
+
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+  return (
+    <Button variant="ghost" size="icon" onClick={toggleTheme} className="hover:bg-transparent">
+      {theme === "light" ? <Moon className="h-4 w-4" /> : <Sun className="h-4 w-4" />}
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
